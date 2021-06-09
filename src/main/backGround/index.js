abb11071b024c7809e19dd48fca09a82c3634706
John <Baozi960101@gmail.com>
2021-06-09 15:28:34 +0800

    first commit

--- src/main/backGround/index.js
+++ src/main/backGround/index.js
@@ -1,5 +1,6 @@
 import { React, useState } from "react";
 import "./index.css";
+import Circle from "./img/circle.svg";
 
 function BackGround({ status }) {
   return (
@@ -7,12 +8,17 @@ function BackGround({ status }) {
       <div className="backGroundLeft"></div>
       <div className="backGroundChangeRight">
         <div className="menuBox">
-          <input className="menuTitleBox" value=""/>
+          <input className="menuTitleBox" value="" />
           <div className="menuMainBox">
-          
+            <div className="menuMainTop"></div>
+            <div className="menuMainProject">
+              <div className="menuMainimg">
+                <img src={Circle} />
+              </div>
+              <div className="menuMainText">5</div>
+            </div>
+          </div>
         </div>
-        </div>
-        
       </div>
     </>
   );
